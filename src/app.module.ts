a5886a0005bcf98e055d39ab440b0b06548e806a
Chakrit <chakritkp@hotmail.com>
2023-12-25 22:15:48 +0700

    finished

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { ObstacleModule } from './obstacle/obstacle.module';
           host: "localhost",
           port: 3306,
           username: "root",
-          password: configService.get('DB_PASSWORD'),
+          password: "1475963",
           database: "prevent_disaster",
           entities: ["dist/**/*.entity{.ts,.js}"],
           synchronize: true
